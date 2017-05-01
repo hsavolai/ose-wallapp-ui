ca504b0eba34afeff7faa65b15b32712d3d8e865
Harri Savolainen <harri.savolainen@gmail.com>
2017-05-01 18:04:29 +0300

    Initial commit

---
+++ src/app/wall-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wall-display',
+  templateUrl: './wall-display.component.html',
+  styleUrls: ['./wall-display.component.css']
+})
+export class WallDisplayComponent {
+  title = 'Wall Display works!';
+}
